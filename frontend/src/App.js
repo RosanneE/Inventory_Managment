101d38af033172875d2f164fa26c1eb6a4c49421
rosanne-a <anderson.rosanne4@gmail.com>
2022-05-17 10:54:21 -0400

    page/component/path update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,14 @@
 //imports
-import InventoryList from './pages/InventoryList';
+import ColorsList from './pages/ColorsList';
 import Nav from './components/Nav';
 // import Footer from './components/Footer';
 import { Route, Routes, Link } from 'react-router-dom';
 import './App.css';
-import Login from './pages/Login';
-import Item from './pages/Item';
+import Main from './pages/Main';
+import Color from './pages/Color';
 import axios from 'axios';
 import React, {useState,useEffect} from "react"
 import SearchResults from './pages/SearchResults';
-import Edit from './pages/Edit';
-import Add from './pages/Add'
 
 function App(props) {
 
@@ -37,11 +35,9 @@ function App(props) {
     <div>
       <Nav/>
     <Routes>
-      <Route path='/' element={<Login />} />
-      <Route path='/products/add' element={<Add />} />
-      <Route path='/products/edit' element={<Edit />} />
-      <Route path='/product/:id' element={<Item products = {products}/>} />
-      <Route path='/products/' element={<InventoryList products = {products}/>} />
+      <Route path='/' element={<Main />} />
+      <Route path='/product/:id' element={<Color products = {products}/>} />
+      <Route path='/products/' element={<ColorsList products = {products}/>} />
       <Route path='/products/results' element={<SearchResults />} />
     </Routes>
     </div>
